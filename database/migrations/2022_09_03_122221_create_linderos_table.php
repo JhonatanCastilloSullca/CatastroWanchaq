f408187d042597af6b3a3c1f003e67d3cce4752b
Jhonatan Castillo Sullca <jcastillosullca@gmail.com>
2024-10-02 13:36:32 -0500

    linderos bd migrations

--- database/migrations/2022_09_03_122221_create_linderos_table.php
+++ database/migrations/2022_09_03_122221_create_linderos_table.php
@@ -16,22 +16,22 @@ return new class extends Migration
         Schema::create('tf_linderos', function (Blueprint $table) {
             $table->string('id_ficha',19)->unsigned();
             $table->foreign('id_ficha')->references('id_ficha')->on('tf_fichas')->onDelete('cascade')->onUpdate('cascade');
-            $table->string('fren_campo',20)->nullable();
-            $table->string('fren_titulo',20)->nullable();
-            $table->string('fren_colinda_campo',20)->nullable();
-            $table->string('fren_colinda_titulo',20)->nullable();
-            $table->string('dere_campo',20)->nullable();
-            $table->string('dere_titulo',20)->nullable();
-            $table->string('dere_colinda_campo',20)->nullable();
-            $table->string('dere_colinda_titulo',20)->nullable();
-            $table->string('izqu_campo',20)->nullable();
-            $table->string('izqu_titulo',20)->nullable();
-            $table->string('izqu_colinda_campo',20)->nullable();
-            $table->string('izqu_colinda_titulo',20)->nullable();
-            $table->string('fond_titulo',20)->nullable();
-            $table->string('fond_campo',20)->nullable();
-            $table->string('fond_colinda_campo',20)->nullable();
-            $table->string('fond_colinda_titulo',20)->nullable();
+            $table->string('fren_campo',200)->nullable();
+            $table->string('fren_titulo',200)->nullable();
+            $table->string('fren_colinda_campo',200)->nullable();
+            $table->string('fren_colinda_titulo',200)->nullable();
+            $table->string('dere_campo',200)->nullable();
+            $table->string('dere_titulo',200)->nullable();
+            $table->string('dere_colinda_campo',200)->nullable();
+            $table->string('dere_colinda_titulo',200)->nullable();
+            $table->string('izqu_campo',200)->nullable();
+            $table->string('izqu_titulo',200)->nullable();
+            $table->string('izqu_colinda_campo',200)->nullable();
+            $table->string('izqu_colinda_titulo',200)->nullable();
+            $table->string('fond_titulo',200)->nullable();
+            $table->string('fond_campo',200)->nullable();
+            $table->string('fond_colinda_campo',200)->nullable();
+            $table->string('fond_colinda_titulo',200)->nullable();
         });        
     }
 
