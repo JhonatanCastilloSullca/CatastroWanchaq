8692fdfbb4bec29015a4d4df335131f24c63bce0
David Miranda <dmirandatarco@gmail.com>
2024-11-18 10:42:07 -0500

    dashboard

--- app/Http/Livewire/FichaIndividualEdit.php
+++ app/Http/Livewire/FichaIndividualEdit.php
@@ -2207,7 +2207,7 @@ class FichaIndividualEdit extends Component
             $isLocal = in_array($host, ['localhost', '192.168.1.16']);
             $mapsUrl = $isLocal ? 'http://192.168.1.16:81' : 'http://209.45.78.210:9101';
 
-            $url = $mapsUrl . "/servicio/wms?service=WMS&request=GetMap&layers=lotes,idLotes,verticesLote,ejeVias&styles=&format=image%2Fpng&transparent=false&version=1.1.1&width=450&height=400&srs=EPSG%3A32719&bbox=" . $extension[0]->extension . "&id=" . $ficha->id_lote;
+            $url = $mapsUrl . "servicio/wms?service=WMS&request=GetMap&layers=lotes,idLotes,verticesLote,ejeVias&styles=&format=image%2Fpng&transparent=false&version=1.1.1&width=450&height=400&srs=EPSG%3A32719&bbox=" . $extension[0]->extension . "&id=" . $ficha->id_lote;
             $nombreArchivo = $ficha->id_ficha . '.jpg';
             if($url){
                 $contenidoImagen = file_get_contents($url); 
