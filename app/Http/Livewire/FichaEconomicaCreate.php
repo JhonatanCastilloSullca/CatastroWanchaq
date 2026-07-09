d4b4fa6e8b9a8a872e177bc0c2021d6dec15d6c0
David Miranda <dmirandatarco@gmail.com>
2026-07-09 09:17:10 -0500

    final

--- app/Http/Livewire/FichaEconomicaCreate.php
+++ app/Http/Livewire/FichaEconomicaCreate.php
@@ -55,9 +55,9 @@ class FichaEconomicaCreate extends Component
     public $razon_social;
     public $cond_conductor;
 
-    public $deparamentoconductor = 'CUSCO';
-    public $provinciaconductor = 'URUBAMBA';
-    public $distritoconductor = 'MACHUPICCHU';
+    public $deparamentoconductor = '';
+    public $provinciaconductor = '';
+    public $distritoconductor = '';
     public $telefonoconductor;
     public $anexoconductor;
     public $faxconductor;
