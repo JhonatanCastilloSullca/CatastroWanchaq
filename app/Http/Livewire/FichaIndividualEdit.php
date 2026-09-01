1efd3509324cd31ac671f6aa1de6374586c02575
David Miranda <dmirandatarco@gmail.com>
2026-09-01 13:55:27 -0500

    final

--- app/Http/Livewire/FichaIndividualEdit.php
+++ app/Http/Livewire/FichaIndividualEdit.php
@@ -2280,7 +2280,7 @@ class FichaIndividualEdit extends Component
                 ) AS subconsulta;
             ");
             
-            $url = env('URL_MAP') . "/servicio/wms?service=WMS&request=GetMap&layers=lotes,idLotes,verticesLote,ejeVias&styles=&format=image%2Fpng&transparent=false&version=1.1.1&width=450&height=400&srs=EPSG%3A32719&bbox=" . $extension[0]->extension . "&id=" . $ficha->id_lote;
+            $url = env('URL_MAP') . "/servicio/wms?service=WMS&request=getMap&version=1.1.1&layers=lote,id_lote,vertice_lote,eje_via&styles=&WIDTH=1680&HEIGHT=834&SRS=EPSG%3A32719&BBOX=" . $extension[0]->extension . "&format=image/png&id=" . $ficha->id_lote;
             $nombreArchivo = $ficha->id_ficha . '.jpg';
             if($url){
                 $contenidoImagen = file_get_contents($url); 
